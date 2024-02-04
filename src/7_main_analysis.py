4ef2937e370ca95351b7a79cc73903180123130c
Yifan Lu <115194735+yifan-lu-300@users.noreply.github.com>
2024-02-04 23:08:06 +0000

    Added code/files for the main_table

--- src/7_main_analysis.py
+++ src/7_main_analysis.py
@@ -60,24 +60,43 @@ xset3 = ['sex_1', 'age_2002', 'age_2002_squared', 'child_in_house_1', 'outside_u
 # # check whether xset3 has abnormally high NA rate
 # tt = sample[xset3].apply(lambda x: x.isna().sum() / len(sample), axis=0)
 
-# Write a for loop to produce the result table
-main_table = pd.DataFrame(np.nan, index=range(18), columns=['Xset1', 'Xset2', 'Xset3'])
+# Calculate propensity score
+ps_formula = f'treatment ~ ' + ' + '.join(xset1)
+ps_model = smf.probit(formula=ps_formula, data=sample).fit()
+sample['ps_xset1'] = ps_model.predict(sample)
+
 y_list = ['any_post', 'angina_heart_attack_diabetes_post'] + [disease + '_post' for disease in disease_list]
 x_list = [xset1, xset2, xset3]
 
 ########## Simple OLS
+ps_table = pd.DataFrame(np.nan, index=range(18), columns=['Xset1', 'Xset2', 'Xset3'])
+# Write a for loop to produce the result table
 for i, xset in enumerate(x_list):
     for j, y in enumerate(y_list):
         model_formula = f'{y} ~ treatment + ' + ' + '.join(xset)
         model = smf.ols(formula=model_formula, data=sample).fit()
 
         # Save results to main_table
-        main_table.iloc[j * 2, i] = model.params['treatment']
-        main_table.iloc[j * 2 + 1, i] = model.tvalues['treatment']
+        ps_table.iloc[j * 2, i] = model.params['treatment']
+        ps_table.iloc[j * 2 + 1, i] = model.tvalues['treatment']
 
-# IV
+########## IV
+# Select neighbourhood around state pension age (women 60 men 65)
+sample_iv = sample.loc[((sample['sex_1'] == 1) & (sample['age_2002'].isin([56, 57, 58, 59, 60]))) |
+                       ((sample['sex_1'] == 0) & (sample['age_2002'].isin([61, 62, 63, 64, 65]))), :]
 
+iv_table = pd.DataFrame(np.nan, index=range(18), columns=['Xset4'])
+# For loop
+for j, y in enumerate(y_list):
+    model_data = sample_iv[[f'{y}', 'sex_1', 'ex_work_1', 'ex_limit_1', 'treatment', 'spage_2']].dropna(subset=[f'{y}'])
+    model_formula = f'{y} ~ 1 + C(sex_1) + C(ex_work_1) + C(ex_limit_1) + [treatment ~ spage_2]'
+    model = IV2SLS.from_formula(model_formula, model_data).fit()
 
+    iv_table.iloc[j * 2, 0] = model.params['treatment']
+    iv_table.iloc[j * 2 + 1, 0] = model.params['treatment'] / model.std_errors['treatment']
 
+########## Concatenate the two tables together
+main_table = pd.concat([ps_table, iv_table], axis=1)
 
-########## Inspection
+########## Save the table
+main_table.to_csv(os.path.join(derived_path, 'main_table.csv'), index=False)
